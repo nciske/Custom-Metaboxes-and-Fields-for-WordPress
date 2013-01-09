f33f755e22b8c7903bef07a471edbda135911d67
Nick Ciske <nick@thoughtrefinery.com>
2013-01-09 00:44:47 -0600

    Allow use with non post pages via filtering meta fetching
    Signed-off-by: Nick Ciske <nick@thoughtrefinery.com>

--- init.php
+++ init.php
@@ -181,7 +181,9 @@ class cmb_Meta_Box {
 			if ( 'file' == $field['type'] && !isset( $field['save_id'] ) )  $field['save_id']  = false;
 			if ( 'multicheck' == $field['type'] ) $field['multiple'] = true;  
 						
-			$meta = get_post_meta( $post->ID, $field['id'], 'multicheck' != $field['type'] /* If multicheck this can be multiple values */ );
+			$pmeta = get_post_meta( $post->ID, $field['id'], 'multicheck' != $field['type'] /* If multicheck this can be multiple values */ );
+
+			$meta = apply_filters( 'cmb_get_post_meta', $pmeta, $post->ID, $field['id'], 'multicheck' != $field['type'] );
 
 			echo '<tr>';
 	
